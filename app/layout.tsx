d9f03007a9cda79c5edd8900b9a87055ecb1aa4b
Jose de Freitas <joselocotote26@gmail.com>
2025-06-12 02:18:07 +0100

    Stylise mainpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from 'next';
+import '@/styles/global.scss';
 
 export const metadata: Metadata = {
-  title: 'Escritura y Lectura',
-  description: 'Sitio web oficial del servidor Escritura y Lectura.',
+    title: 'Escritura y Lectura',
+    description: 'Sitio web oficial del servidor Escritura y Lectura.',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="es">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="es">
+            <body>{children}</body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import Link from 'next/link'
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Sitio web oficial del servidor Escritura y Lectura.</h1>
-      <div>
-        Páginas:
-          <ul>
-            <li><Link href="/biblioteca">Biblioteca</Link></li>
-          </ul>
-      </div>
-    </main>
-  );
+    const router = useRouter();
+
+    const irAPagina = (pagina: string) => {
+        router.push(pagina);
+    }
+
+    return (
+        <main>
+            <h1>Sitio web oficial del servidor Escritura y Lectura</h1>
+            <div id="contenedor-paginas">
+                <div className="contenedor-subpaginas">
+                    <h2 onClick={() => irAPagina('/biblioteca')}>Biblioteca</h2>
+                    <div>
+                        <p onClick={() => irAPagina('/biblioteca/buscador')}>Buscador</p>
+                        <p onClick={() => irAPagina('/biblioteca/indices')}>Índices</p>
+                        <p onClick={() => irAPagina('/biblioteca/estadisticas')}>Estadísticas</p>
+                    </div>
+                </div>
+            </div>
+            <p><a href="https://github.com/EscrituraYLectura">GitHub</a></p>
+        </main>
+    );
 }
