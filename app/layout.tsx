38bbcf310e42d39796f72f179647a06886d14844
Jose de Freitas <joselocotote26@gmail.com>
2025-06-12 02:54:39 +0100

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import '@/styles/global.scss';
 export const metadata: Metadata = {
     title: 'Escritura y Lectura',
     description: 'Sitio web oficial del servidor Escritura y Lectura.',
+    icons: {
+        icon: '/icon.ico',
+    },
 };
 
 export default function RootLayout({
