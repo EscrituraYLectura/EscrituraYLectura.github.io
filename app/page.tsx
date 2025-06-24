3e26b1eb11ecfd756a0e6234a71e64da45897fd3
Jose de Freitas <joselocotote26@gmail.com>
2025-06-24 18:42:43 +0100

    Separate code in topbar and subbar and add icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Home() {
     return (
         <main>
-            <h1>Sitio web oficial del servidor Escritura y Lectura</h1>
+            <div id="barra-superior">
+                <img
+                src='./icon.png'
+                alt='Logo de Escritura y Lectura'
+                width={42}
+                height={42}
+                />
+                <h1>Sitio web oficial del servidor Escritura y Lectura</h1>
+            </div>
             <div id="contenedor-paginas">
                 <div className="contenedor-subpaginas">
                     <Link className="contenedor-titulo" href="/biblioteca">Biblioteca</Link>
@@ -17,7 +24,9 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <p><a href="https://github.com/EscrituraYLectura">GitHub</a></p>
+            <div id="barra-inferior">
+                <p><a href="https://github.com/EscrituraYLectura">GitHub</a></p>
+            </div>
         </main>
     );
 }
